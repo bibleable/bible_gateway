04cd95489348f5292b054f7897181024b17c939b
Geoffrey Dagley <gdagley@gmail.com>
2014-07-01 23:25:18 -0500

    Convert specs to RSpec 3.0.2 syntax with Transpec
    This conversion is done by Transpec 2.3.1 with the following command:
        transpec
    
    * 16 conversions
        from: obj.should
          to: expect(obj).to
    
    * 6 conversions
        from: == expected
          to: eq(expected)
    
    * 2 conversions
        from: lambda { }.should
          to: expect { }.to
    
    * 1 conversion
        from: obj.should_not
          to: expect(obj).not_to
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/bible_gateway_spec.rb
+++ spec/bible_gateway_spec.rb
@@ -3,33 +3,33 @@ require "spec_helper"
 
 describe BibleGateway do
   it "should have a list of versions" do
-    BibleGateway.versions.should_not be_empty
+    expect(BibleGateway.versions).not_to be_empty
   end
 
   describe "setting the version" do
     it "should have a default version" do
       gateway = BibleGateway.new
-      gateway.version.should == :king_james_version
+      expect(gateway.version).to eq(:king_james_version)
     end
 
     it "should be able to set the version to use" do
       gateway = BibleGateway.new :english_standard_version
-      gateway.version.should == :english_standard_version
+      expect(gateway.version).to eq(:english_standard_version)
     end
 
     it "should raise an exception for unknown version" do
-      lambda { BibleGateway.new :unknown_version }.should raise_error(BibleGatewayError)
+      expect { BibleGateway.new :unknown_version }.to raise_error(BibleGatewayError)
     end
 
     it "should be able to change the version to use" do
       gateway = BibleGateway.new
       gateway.version = :english_standard_version
-      gateway.version.should == :english_standard_version
+      expect(gateway.version).to eq(:english_standard_version)
     end
 
     it "should raise an exception when changing to unknown version" do
       gateway = BibleGateway.new
-      lambda {  gateway.version = :unknown_version }.should raise_error(BibleGatewayError)
+      expect {  gateway.version = :unknown_version }.to raise_error(BibleGatewayError)
     end
   end
 
@@ -37,43 +37,43 @@ describe BibleGateway do
     context "verse" do
       it "should find the passage title" do
         title = BibleGateway.new(:english_standard_version).lookup("John 1:1")[:title]
-        title.should == "John 1:1"
+        expect(title).to eq("John 1:1")
       end
 
       it "should find and clean the passage content" do
         content = BibleGateway.new(:english_standard_version).lookup("John 1:1")[:content]
-        content.should include("<h3>The Word Became Flesh</h3>")
-        content.should include("In the beginning was the Word, and the Word was with God, and the Word was God.")
+        expect(content).to include("<h3>The Word Became Flesh</h3>")
+        expect(content).to include("In the beginning was the Word, and the Word was with God, and the Word was God.")
       end
     end
 
     context "chapter" do
       it "should find the passage title" do
         title = BibleGateway.new(:english_standard_version).lookup("John 3")[:title]
-        title.should == "John 3"
+        expect(title).to eq("John 3")
       end
 
       it "should find and clean the passage content" do
         content = BibleGateway.new(:english_standard_version).lookup("John 3")[:content]
-        content.should include("<h3>You Must Be Born Again</h3>")
-        content.should include("<h3>For God So Loved the World</h3>")
-        content.should include("For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life.")
+        expect(content).to include("<h3>You Must Be Born Again</h3>")
+        expect(content).to include("<h3>For God So Loved the World</h3>")
+        expect(content).to include("For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life.")
       end
     end
 
     context "multiple chapters" do
       it "should find the passage title" do
         title = BibleGateway.new(:english_standard_version).lookup("Psalm 1-5")[:title]
-        title.should == "Psalm 1-5"
+        expect(title).to eq("Psalm 1-5")
       end
 
       it "should find and clean the passage content" do
         content = BibleGateway.new(:english_standard_version).lookup("Psalm 1-5")[:content]
-        content.should include("<h3>The Way of the Righteous and the Wicked</h3>")
-        content.should include("<span class=\"chapternum\">1 </span>")
-        content.should include("<h3>Save Me, O My God</h3>")
-        content.should include("<span class=\"chapternum\">1 </span>")
-        content.should include("For you are not a God who delights in wickedness;")
+        expect(content).to include("<h3>The Way of the Righteous and the Wicked</h3>")
+        expect(content).to include("<span class=\"chapternum\">1 </span>")
+        expect(content).to include("<h3>Save Me, O My God</h3>")
+        expect(content).to include("<span class=\"chapternum\">1 </span>")
+        expect(content).to include("For you are not a God who delights in wickedness;")
       end
     end
   end
